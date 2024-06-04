691339734070894a15ee013a14c8b8c0fc05301b
Joelmir Uchoa <joelmiruchoa@gmail.com>
2024-06-04 14:38:52 -0300

    Condicional encadeada

--- controle-fluxo/src/ResultadoEscolar.java
+++ controle-fluxo/src/ResultadoEscolar.java
@@ -1,10 +1,14 @@
 public class ResultadoEscolar {
     public static void main(String[] args) {
-        int nota = 7 ;
+        int nota = 5 ;
 
         if (nota >= 7) {
             System.out.println("Aprovado");
-        } else {
+
+        }else if (nota >= 5 && nota < 7) {
+            System.out.println("Recuperação");
+
+        }else {
             System.out.println("Reprovado");
         }
     }
