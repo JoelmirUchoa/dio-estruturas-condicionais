1f5620a06c695fcc47cc02969e6f7b3cbe944334
Joelmir Uchoa <joelmiruchoa@gmail.com>
2024-06-04 16:09:47 -0300

    Switch Case

---
+++ controle-fluxo/src/SistemaMedida.java
@@ -0,0 +1,34 @@
+public class SistemaMedida {
+    public static void main(String[] args) {
+        String sigla = "A";
+        
+        //Switch Case
+        switch (sigla) {
+            case "P":{
+                System.out.println("PEQUENO");
+                break;
+            }
+            case "M":{
+                System.out.println("PEQUENO");
+                break;
+            }
+            case "G":{
+                System.out.println("PEQUENO");
+                break;
+            }
+            default:
+            System.out.println("Indefinido");
+        }
+
+        //Composta
+        /*if (sigla == "P") {
+            System.out.println("Pequeno");
+        } else if (sigla == "M") {
+            System.out.println("Medio");
+        } else if (sigla == "G") {
+            System.out.println("Grande");
+        } else {
+            System.out.println("Indefinido");
+        }*/
+    }
+}
